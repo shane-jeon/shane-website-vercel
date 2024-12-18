41a56dfc6b3ec3b3d9841025e0e260557e708bf3
Shane Jeon <88866019+codingtokki@users.noreply.github.com>
2024-12-18 14:39:46 -0800

    initial commit, with initial styling changes and header, hero components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
+import Hero from "./components/Hero";
+
 export default function Home() {
   return (
-    <div className="flex h-screen flex-col items-center justify-center">
-      <h1 className="text-xl">
+    <div className="flex h-screen flex-col items-center justify-center bg-gradient-to-b from-[#F1EAFF] via-[#E2D5FD] to-[#AECDFC]">
+      <Hero />
+      {/* <h1 className="text-xl ">
         Undergoing reconstruction. Thank you for your understanding!
       </h1>
-      <h2 className="mt-10">-Shane J.</h2>
+      <h2 className="mt-10">-Shane J.</h2> */}
     </div>
   );
 }
